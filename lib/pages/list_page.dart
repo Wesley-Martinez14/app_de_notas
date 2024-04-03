d43c60e81f992120b0741699583950902fe5d65a
Wesley-Martinez14 <riwes1403@gmail.com>
2024-04-03 19:50:50 -0400

    Vistas de la app de notas

---
+++ lib/pages/list_page.dart
@@ -0,0 +1,55 @@
+import 'package:app_de_notas/pages/save_page.dart';
+import 'package:flutter/material.dart';
+
+class ListPage extends StatelessWidget {
+
+  static const String ROUTE = "/";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(child: Icon(Icons.add), onPressed: (){
+        Navigator.pushNamed(context, SavePage.ROUTE);
+      },),
+      appBar: AppBar( 
+        title: Text("Listado"),
+        ),
+      body: Container(
+        child: ListView(
+          children: <Widget>[
+        ListTile(
+          title: Text("Nota 1"),
+          ),
+        ListTile(
+          title: Text("Nota 1"),
+          ),
+        ListTile(
+          title: Text("Nota 1"),
+          ),
+        ListTile(
+          title: Text("Nota 1"),
+          ),
+        ListTile(
+          title: Text("Nota 1"),
+          ),
+        ListTile(
+          title: Text("Nota 1"),
+          ),
+        ListTile(
+          title: Text("Nota 1"),
+          ),
+        ListTile(
+          title: Text("Nota 1"),
+          ),
+        ListTile(
+          title: Text("Nota 1"),
+          ),
+        ListTile(
+          title: Text("Nota 1"),
+          ),
+        ListTile(
+          title: Text("Nota 1"),
+          ),
+      ],),)
+    );
+  }
+}
---
+++ lib/pages/save_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class SavePage extends StatelessWidget {
+
+  static const String ROUTE = "/save";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text("Guardar"),),
+    body: Container(
+      child: _FormSave(),),
+      );
+  }
+}
+
+class _FormSave extends StatelessWidget {
+
+
+final _formKey = GlobalKey<FormState>();
+final titleController = TextEditingController();
+final contentController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(15),
+      child: Form(
+        key: _formKey,
+        child: Column(children: <Widget>[
+          TextFormField(
+            controller: titleController,
+            validator: (value){
+              if (value!.isEmpty) {
+                return "Tiene que colocar datos";
+              }
+              return null;
+            },
+            decoration: InputDecoration(
+              labelText: "Titulo",
+              border: OutlineInputBorder() //borderRadius: BorderRadius.all(Radius.circular(50))
+            ),
+          ),
+          SizedBox(height: 15,),
+          TextFormField(
+            controller: contentController,
+            maxLines: 8,
+            maxLength: 1000,
+            validator: (value){
+              if (value!.isEmpty) {
+                return "Tiene que colocar datos";
+              }
+              return null;
+            },
+            decoration: InputDecoration(
+              labelText: "Contenido",
+              border: OutlineInputBorder() //borderRadius: BorderRadius.all(Radius.circular(50))
+            ),
+          ),
+          ElevatedButton(child: Text("Guardar"), 
+          onPressed: (){
+            if(_formKey.currentState!.validate()){
+              print("Valido" +titleController.text);
+            }
+          })
+        ],),
+      ),
+    );
+  }
+}
