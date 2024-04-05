27d7867db5af9b407a44d59b976dedc66d248ea2
Alejandro202010329 <110682861+Alejandro202010329@users.noreply.github.com>
2024-04-05 16:42:34 -0400

    PArte 10-13 completada se agrego la base de datos y la tabla nota

---
+++ lib/models/note.dart
@@ -0,0 +1,19 @@
+class Note {
+  final int id;
+  final String title;
+  final String content;
+
+  Note({
+    required this.id, 
+    required this.title, 
+    required this.content
+  });
+
+  Map<String, Object> toMap(){
+  return {
+      'id': id,
+      'title': title,
+      'content': content,
+    };
+  }
+}
